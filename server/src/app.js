f9d03e9850f9c2df33ff8d7368bd76a94500773f
Israr Ahmad khan <126082687+Israr9650@users.noreply.github.com>
2026-08-16 13:18:56 +0530

    updated client url

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173"
+    origin: process.env.CLIENT_URL || "https://datastraw-support-crm-2f44.vercel.app"
   })
 );
 
